7f339a6a29f9ddedf52c772e944eee4b609a3e6e
Kelly <rkm@Kellys-iMac.local>
2020-11-10 02:24:54 -0600

    4th commit, finished step 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,14 +4,27 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    //warning: boolean;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean, /*warning: boolean*/) {
         this.name = name;
         this.orbitType = orbitType;
         this.type = type;
         this.operational = operational;
         this.launchDate = launchDate;
+        //this.warning = warning;
     }
+
+//<td [class.warning]="satellite.shouldShowWarning()">{{satellite.type}}</td>
+
+    shouldShowWarning() {
+        if(this.type === 'Space Debris' || this.type === 'space debris'){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
         
 
 
